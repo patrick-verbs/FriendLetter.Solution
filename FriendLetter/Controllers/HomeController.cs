11841c9088fd91b652511733b1e55f8258c1f112
TorchAblaze <tiffanygreathead@gmail.com>
2021-05-09 22:50:14 -0700

    Add Razor and cool letter form stuff
    Co-authored-by: Cristina Plesa <cristinaplesa8@gmail.com>
    Co-authored-by: Patrick Lee <patricklee1138@gmail.com>
    Co-authored-by: Seth Medeiros <MedeirosSeth@gmail.com>

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using FriendLetter.Models;
 
 namespace FriendLetter.Controllers
 {
@@ -11,8 +12,26 @@ namespace FriendLetter.Controllers
     public string Goodbye() { return "Goodbye friend."; }
 
     [Route("/")]
-    public string Letter() { return "Our virtual postcard will go here soon!"; }
+    public ActionResult Letter() 
+    {  
+      LetterVariable myLetterVariable = new LetterVariable();
+      myLetterVariable.Recipient = "Cristina, Patrick, Seth";
+      myLetterVariable.Sender = "Tiffany the Bloodthirsty";
+      return View(myLetterVariable);
+    }
 
+    [Route("/form")]
+    public ActionResult Form() { return View(); }
+
+    [Route("/postcard")]
+    public ActionResult Postcard(string recipient, string sender)
+    {
+      LetterVariable myLetterVariable = new LetterVariable();
+      myLetterVariable.Recipient = recipient;
+      myLetterVariable.Sender = sender;
+      return View(myLetterVariable);
+    }
+    
     [Route("/hamlet")]
     public string Hamlet() { return @"To be, or not to be, that is the question:
     Whether 'tis nobler in the mind to suffer
